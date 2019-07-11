c44814500d83b0beb4b7c88157b84301a9285065
garakoong <45497379+garakoong@users.noreply.github.com>
2019-07-11 13:36:54 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,68 @@
 const baseURL = "https://exceed.superposition.pknn.dev/data/2gorillas"
 
-function getData(id) {
+
+setInterval( () => {
     fetch(baseURL)
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            if(data["door"] == "close"){
+                document.getElementById("door").innerHTML = "open";
+                document.getElementById("doorStat").innerHTML = "close";
+            }
+            else {
+                document.getElementById("door").innerHTML = "close";
+                document.getElementById("doorStat").innerHTML = "open";
+            }
 
-    fetch(baseURL + "/" + id)
+            if(data["light"] == "off"){
+                document.getElementById("light").innerHTML = "on";
+                document.getElementById("lightStat").innerHTML = "off";
+            }
+            else {
+                document.getElementById("light").innerHTML = "off";
+                document.getElementById("lightStat").innerHTML = "on";
+            }
+            
+            document.getElementById("buzzerStat").innerHTML = data["buzzer"];
+        })
+
+    /*fetch(baseURL + "/" + id)
         .then((res) => res.json())
         .then((data) => console.log(data))
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err))*/
+}, 1000)
+
+function postData() {
+    let url = baseURL;
+    let data = {
+            "data": {
+                "door": "close",
+                "light": "off",
+                "buzzer": "off"
+            }
+    }
+    
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })
 }
+
+function putData(id) {
+    let url = baseURL + "/" + id;
+    var data = {
+            "value": document.getElementById(id).innerHTML
+    }
+    
+    fetch(url, {
+        method: 'PUT', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })
+}
+
